6185e36562f9ab0fd848f270048e464c76cc4a0d
skutukov <skutukov96@gmail.com>
2017-04-27 00:26:18 +0300

    4

--- 1/4/main.cpp
+++ 1/4/main.cpp
@@ -4,43 +4,65 @@
 #include <vector>
 
 using namespace std;
-//int dfs(int v,vector<vector<int>> A,vector<bool> used) {
+int MAXN=100000;
+int N,M;
+enum COLOR{WHITE,GRAY,BLACK};
+vector<vector<int> > adj;
+vector<COLOR> used(MAXN);
+bool isTopExist=true;
+void dfs(int cur, vector<int> &res) {
+    used[cur] = GRAY;
+  for (int next :adj[cur]) {
+    if (used[next] == GRAY)
+        {
+             isTopExist=false;
+             return;
+        }
+    if (used[next] == WHITE)
+      dfs(next,res);
+  }
+  used[cur] = BLACK;
+  res.push_back(cur);
+}
+void topsort(vector<int> &res) {
+  for (int i=0;i<N;i++) {
+    if (used[i] == WHITE) {
+        dfs(i,res);
+      }
+    }
+}
 
-//    int n=0;
-//    if (used[v]) {
-//        return n;
-//    }
-//    n+=1;
-//    used[v] = true;
-//    for (auto i:A[v])
-//    {
-//        n+=dfs(i,A,used);
-//    }
-//    return n;
-//}
+
 int main(int argc, char *argv[])
 {
-    vector<vector<int>> A;
-    int N,M;
     ///---------------input data ---------------
     ifstream in("topsort.in");
     in>>N>>M;
-    A.resize(N);
+    adj.resize(N);
     for(int i=0;i<M;i++)
     {
         int a,b;
         in>>a>>b;
         a--;
         b--;
-        A[a].push_back(b);
-        A[b].push_back(a);
+        adj[a].push_back(b);
     }
     in.close();
     ///------------topsort -------------
+    vector<int> res;
+    topsort(res);
     ///------------output data -----------
     ofstream out("topsort.out");
-    int i=-1;
-    out << i;
+    if(isTopExist)
+    {
+        for(int i=res.size()-1;i>=0;i--)
+            {
+                std::cout<<res[i]+1<<std::endl;
+                out <<res[i]+1<<' ';
+            }
+    }else{
+        out << "-1";
+    }
     ///------release memory ---------------
     out.close();
     return 0;
