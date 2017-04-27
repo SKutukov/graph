3758ef6c74b07cd214eb0dd0af74ff0e57adad80
Sergey Kutukov <Sergey.Kutukov@lanit-tercom.com>
2017-04-27 13:54:55 +0300

    5

---
+++ 1/5/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int MAXN=100000;
+int N,M;
+
+vector<vector<int> > adj;
+void dfs(int cur, int &n) {
+  int max_d=0;
+  for (int next :adj[cur]) {
+        int d=1;
+        dfs(next,d);
+        max_d=std::max(d,max_d);
+  }
+  n+=max_d;
+}
+int longpath() {
+  int max=0;
+  for (int i=0;i<N;i++) {
+        int n=0;
+        dfs(i,n);
+        max=std::max(max,n);
+    }
+  return max;
+}
+
+
+int main(int argc, char *argv[])
+{
+    ///---------------input data ---------------
+    ifstream in("longpath.in");
+    in>>N>>M;
+    adj.resize(N);
+    for(int i=0;i<M;i++)
+    {
+        int a,b;
+        in>>a>>b;
+        a--;
+        b--;
+        adj[a].push_back(b);
+    }
+    in.close();
+    ///------------topsort -------------
+    int n=longpath();
+    ///------------output data -----------
+    ofstream out("longpath.out");
+    out<<n;
+    ///------release memory ---------------
+    out.close();
+    return 0;
+}
+
